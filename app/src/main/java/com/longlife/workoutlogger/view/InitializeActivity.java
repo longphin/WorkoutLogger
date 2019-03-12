29e86f47d8ce04aba6be726a88f94816c227f355
Longphi Nguyen <nguyenl707@gmail.com>
2019-03-11 22:02:15 -0700

    Added read/write permission check before activity.

--- app/src/main/java/com/longlife/workoutlogger/view/InitializeActivity.java
+++ app/src/main/java/com/longlife/workoutlogger/view/InitializeActivity.java
@@ -6,7 +6,9 @@
 
 package com.longlife.workoutlogger.view;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.longlife.workoutlogger.MyApplication;
@@ -17,7 +19,10 @@ import com.longlife.workoutlogger.view.Profile.ProfileViewModel;
 
 import javax.inject.Inject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -25,6 +30,37 @@ public class InitializeActivity extends AppCompatActivity {
     @Inject
     public ViewModelProvider.Factory viewModelFactory;
     private ProfileViewModel profileViewModel;
+
+    private static final int MY_PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 0;
+    private static final int MY_PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 1;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // If the request was canceled, the result arrays are empty.
+        boolean permissionWasGranted = (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED);
+
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                if (permissionWasGranted) {
+                    // Permission was granted. Continue.
+                    checkWritePermissionSuccess();
+                } else {
+                    // Permission was denied.
+                }
+                return;
+            }
+            case MY_PERMISSION_REQUEST_READ_EXTERNAL_STORAGE: {
+                if (permissionWasGranted) {
+                    checkReadPermissionSuccess();
+                } else {
+                    // Permission was denied.
+                }
+                return;
+            }
+
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +72,39 @@ public class InitializeActivity extends AppCompatActivity {
                 .inject(this);
         profileViewModel = ViewModelProviders.of(this, viewModelFactory).get(ProfileViewModel.class);
 
+        checkWritePermission();
+    }
+
+    private void checkWritePermission() {
+        // Check if app has read/write permission for local storage database.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Permission was not granted.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE);
+        } else {
+            checkWritePermissionSuccess();
+        }
+    }
+
+    private void checkWritePermissionSuccess() {
+        checkReadPermission();
+    }
+
+    private void checkReadPermission() {
+        // Check if app has read/write permission for local storage database.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Permission was not granted.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST_READ_EXTERNAL_STORAGE);
+        } else {
+            checkReadPermissionSuccess();
+        }
+    }
+
+    private void checkReadPermissionSuccess() {
+        permissionsGranted();
+    }
+
+    // If proper permissions were granted, then continue the app.
+    private void permissionsGranted() {
         // Insert muscle tables.
         profileViewModel.insertMuscles(Muscle.getAllMuscleEntities(this));
         profileViewModel.insertMuscleGroups(MuscleGroup.getAllMuscleGroupEntities(this));
